fix encoding, optimizer and precision

internal/modules/encoding/tcnn_hash_grid.py
@@ -44,7 +44,7 @@ class TCNNHashGrid(Encodings):
         if not torch.all(x <= box_max) or not torch.all(x >= box_min):
             print("ALERT: some points are outside bounding box.")
 
-        x = (x + bound) / (2 * bound)  # normalize to [0., 1.]
+        x = (x - box_min) / bound  # normalize to [0., 1.]
         return self.encoder(x)
 
     def get_output_n_channels(self) -> int:

internal/optimizer.py
@@ -7,8 +7,16 @@ def get_optimizer(models: list, hparams: dict):
     for model in models:
         parameters += list(model.parameters())
 
-    eps = 1e-8
-    optimizer = torch.optim.Adam(parameters, lr=hparams["lrate"], eps=eps)
+    extra_optimizer_params = {
+        "eps": 1e-8,
+    }
+
+    if "betas" in hparams:
+        extra_optimizer_params["betas"] = (hparams["betas"][0], hparams["betas"][1])
+    if "eps" in hparams:
+        extra_optimizer_params["eps"] = float(hparams["eps"])
+
+    optimizer = torch.optim.Adam(parameters, lr=hparams["lrate"], **extra_optimizer_params)
 
     return optimizer
 